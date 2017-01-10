2662e4185e1010d7964cd5d2fb54e55079d8c6ea
wangying <wangying@twotiger.com>
2017-01-10 18:15:49 +0800

    compile wilddog sdk

--- app/src/main/java/com/liz/wangying/sosweet/MainActivity.java
+++ app/src/main/java/com/liz/wangying/sosweet/MainActivity.java
@@ -48,6 +48,9 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
             //配置ssh测试
+            //配置ssh测试
+            //配置ssh测试
+
         // 初始化
         WilddogOptions options = new WilddogOptions.Builder().setSyncUrl("https://sosweet.wilddogio.com").build();
         WilddogApp.initializeApp(this, options);
