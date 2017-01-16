fc288cc8a163de77bbe4fb313750aa30bf05c748
liz <lizwangying@icloud.com>
2017-01-16 18:43:53 +0800

    add svg animation

--- app/src/main/java/com/liz/wangying/sosweet/DetailActivity.java
+++ app/src/main/java/com/liz/wangying/sosweet/DetailActivity.java
@@ -1,11 +1,12 @@
 package com.liz.wangying.sosweet;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+import com.github.jorgecastillo.FillableLoader;
+import com.github.jorgecastillo.listener.OnStateChangeListener;
+import com.liz.wangying.sosweet.utils.LogoPaths;
+
 /**
  *          .,:,,,                                        .::,,,::.
  *        .::::,,;;,                                  .,;;:,,....:i:
@@ -44,23 +45,53 @@ import android.view.View;
  *                    ..:isssssrrii;::::::;;iirsssssr;:..
  *                         .,::iiirsssssssssrri;;:.
  */
-public class DetailActivity extends AppCompatActivity {
-
+public class DetailActivity extends AppCompatActivity implements OnStateChangeListener,ResettableView {
+//    @Bind(R.id.fillableLoader)@Nullable  FillableLoader fillableLoader;
+FillableLoader fillableLoader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        ButterKnife.bind(this);
         setContentView(R.layout.activity_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+         fillableLoader = (FillableLoader) findViewById(R.id.fillableLoader);
+//        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "ff", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+//        View rootview = LayoutInflater.from(DetailActivity.this).inflate(R.layout.activity_detail, null);
+//        FillableLoaderBuilder loaderBuilder = new FillableLoaderBuilder();
+//        fillableLoader = loaderBuilder.parentView((RelativeLayout) rootview)
+//                .svgPath(LogoPaths.WONENG_LC_LOGO_PATH)
+//                .strokeDrawingDuration(2000)
+//                .clippingTransform(new WavesClippingTransform())
+//                .fillDuration(10000)
+//                .layoutParams(params)
+//                .build();
+        fillableLoader.setOnStateChangeListener(DetailActivity.this);
+        fillableLoader.setSvgPath(LogoPaths.WONENG_LC_LOGO_PATH);
+        fillableLoader.setOnStateChangeListener(this);
+        fillableLoader.start();
     }
 
+    @Override
+    public void onStateChange(int state) {
+//        showsta
+    }
+
+    @Override
+    public void reset() {
+        fillableLoader.postDelayed(new Runnable() {
+            @Override public void run() {
+                fillableLoader.start();
+            }
+        }, 250);
+    }
 }
---
+++ app/src/main/java/com/liz/wangying/sosweet/ResettableView.java
@@ -0,0 +1,10 @@
+package com.liz.wangying.sosweet;
+
+/**
+ * desc:
+ * Created by Liz on 2017/1/16.
+ * github: https://github.com/lizwangying
+ */
+public interface ResettableView {
+    void reset();
+}
